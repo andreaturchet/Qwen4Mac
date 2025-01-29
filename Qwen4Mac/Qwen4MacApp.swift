589b601918f1902e26f0672bd579cac58c557c8e
Andrea Turchet <andre.turky@gmail.com>
2025-01-29 23:47:58 +0100

    Initial Commit

---
+++ Qwen4Mac/Qwen4MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qwen4MacApp.swift
+//  Qwen4Mac
+//
+//  Created by andrea turchet on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Qwen4MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
