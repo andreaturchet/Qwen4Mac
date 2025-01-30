7efec4134482a63dc69acfcfdc0220ca80514c77
Andrea Turchet <andre.turky@gmail.com>
2025-01-30 13:28:37 +0100

    bug found: github and google login issue
    actually the webview has some problem logging in via google or github

--- Qwen4Mac/MainUI.swift
+++ Qwen4Mac/MainUI.swift
@@ -12,7 +12,8 @@ struct MainUI: View {
     private let webAddress = "https://chat.qwenlm.ai"
 
     private var webConfig: WebViewConfig {
-        WebViewConfig(customUserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15")
+        WebViewConfig(//customUserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
+        )
     }
 
     var body: some View {
